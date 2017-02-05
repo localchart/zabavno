f88928daf7ee87232cc5448557c8c255d5f338af
Göran Weinholt <goran@weinholt.se>
2017-02-05 18:08:34 +0100

    Use scheme-ci in .travis.yml

--- run-tests.sh
+++ run-tests.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 set -ex
 
-# Runs tests. Should be called from a CI system (e.g. .travis.yml);
-# alternatively Chez Scheme can be used and $SCHEME can be set to
-# ChezScheme: SCHEME=ChezScheme ./run-tests.sh
-
-RUNSCHEME="${RUNSCHEME-}"
-
 function cleanup {
   rm -f generate.out
 }
 trap cleanup EXIT
 
 # Check that the programs can be started at all.
-$RUNSCHEME programs/zabavno --help
-$RUNSCHEME programs/zabavno-dos
+programs/zabavno --help
+programs/zabavno-dos
 
 # The instruction tester.
 case $(uname -m) in
     x86_64|i386)
         case "$SCHEME" in
             ChezScheme)
-                $RUNSCHEME tests/x86/generate.sps && chmod +x generate.out
+                tests/x86/generate.sps && chmod +x generate.out
                 ./generate.out
         esac
 esac
